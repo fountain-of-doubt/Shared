d4cc8d04e755593b7c9790cbeb5fa8046d03d449
fountain-of-doubt <kszaki2@freemail.hu>
2023-08-21 17:50:45 +0200

    Add more extension methods

---
+++ Shared/Functional/ResultTEExtensions_Tap.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Functional;
+
+public static partial class ResultTEExtensions
+{
+    public static Result<TError, TSuccess> Tap<TError, TSuccess>(in this Result<TError, TSuccess> result, Action<TSuccess> tap)
+    {
+        if (result.IsSuccess) { tap(result.SuccessContent); }
+        return result;
+    }
+}
